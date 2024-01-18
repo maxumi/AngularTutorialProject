94768379dcdfa45fcef757e92c6d727bd7e015d0
maxumi <maxjac64@gmail.com>
2024-01-18 08:40:04 +0100

    Add the search feature to your app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,10 +13,17 @@ import { HousingLocation } from '../housinglocation';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
-
+  filteredLocationList: HousingLocation[] = [];
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
-  
-
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
+  }
 }
